ac044dea03eecd24a7c0a21bbbd1692af1b63cb4
qwer0114 <skyok203@naver.com>
2025-04-15 20:00:42 +0900

    feat: router address add

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MealForecastPage from "./page/MealForecastPage";
 import WastePage from "./page/WastePage";
+import MealForecastCalendar from "./page/MealForecastCalendar";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/waste" element={<WastePage />} />
-        <Route path="/meal-forecast" element={<MealForecastPage />} />
+        <Route
+          path="/team01/meal-forecast-calendar"
+          element={<MealForecastCalendar />}
+        />
+        <Route path="/team01/meal-forecast" element={<MealForecastPage />} />
       </Routes>
     </BrowserRouter>
   );
