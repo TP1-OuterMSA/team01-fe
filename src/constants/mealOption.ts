6235c5a2d78fc80a30b078116d0c7b6c0d3f67b1
qwer0114 <rnalgh0114@gmail.com>
2025-04-27 12:56:00 +0900

    feat: 식권 옵션 상수 정의

---
+++ src/constants/mealOption.ts
@@ -0,0 +1,23 @@
+import { MealOption } from "../type/meal";
+const mealOptions: MealOption[] = [
+  {
+    id: 0,
+    name: "학생식당 조식",
+    price: 1000,
+    icon: "🍛",
+  },
+  {
+    id: 1,
+    name: "학생식당 중식",
+    price: 6500,
+    icon: "🍜",
+  },
+  {
+    id: 2,
+    name: "학생식당 석식",
+    price: 6500,
+    icon: "🍲",
+  },
+];
+
+export { mealOptions };
