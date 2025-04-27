2fd4e173d5d358419208727fc5300ad16b8d426d
qwer0114 <rnalgh0114@gmail.com>
2025-04-27 12:54:14 +0900

    feat: 식권 구매 및 결제 페이지 라우팅 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MealForecastPage from "./page/MealForecastPage";
 import WastePage from "./page/WastePage";
 import MealForecastCalendar from "./page/MealForecastCalendar";
+import PayPage from "./page/pay/PayPage";
+import Success from "./page/pay/Success";
+import Fail from "./page/pay/Fail";
+import MealTicket from "./page/MealTicket";
 
 const Router = () => {
   return (
@@ -10,6 +14,10 @@ const Router = () => {
         <Route path="/waste" element={<WastePage />} />
         <Route path="/team1" element={<MealForecastCalendar />} />
         <Route path="/team1/meal-forecast" element={<MealForecastPage />} />
+        <Route path="/team1/mealTicket" element={<MealTicket />} />
+        <Route path="/team1/pay" element={<PayPage />} />
+        <Route path="/team1/pay/success" element={<Success />} />
+        <Route path="/team1/pay/fail" element={<Fail />} />
       </Routes>
     </BrowserRouter>
   );
