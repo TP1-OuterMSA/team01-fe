53145ac2767974f49832f6a301177af1412d536b
si14444 <si14444@naver.com>
2025-04-29 02:20:13 +0900

    feat: 네비게이션 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MealForecastPage from "./page/MealForecastPage";
-import WastePage from "./page/WastePage";
+import Navigation from "./components/Navigation";
 import MealForecastCalendar from "./page/MealForecastCalendar";
+import MealForecastPage from "./page/MealForecastPage";
+import MealTicket from "./page/MealTicket";
+import Fail from "./page/pay/Fail";
 import PayPage from "./page/pay/PayPage";
 import Success from "./page/pay/Success";
-import Fail from "./page/pay/Fail";
-import MealTicket from "./page/MealTicket";
+import WastePage from "./page/WastePage";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="font-pretendard flex min-h-screen bg-gray-100">
+      <Navigation />
+      <div className="flex-grow ml-64 p-6">
+        <div className="bg-white rounded-lg shadow-md p-6 min-h-full">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Router = () => {
   return (
@@ -13,11 +27,46 @@ const Router = () => {
       <Routes>
         <Route path="/waste" element={<WastePage />} />
         <Route path="/team1" element={<MealForecastCalendar />} />
-        <Route path="/team1/meal-forecast" element={<MealForecastPage />} />
-        <Route path="/team1/mealTicket" element={<MealTicket />} />
-        <Route path="/team1/pay" element={<PayPage />} />
-        <Route path="/team1/pay/success" element={<Success />} />
-        <Route path="/team1/pay/fail" element={<Fail />} />
+        <Route
+          path="/team1/meal-forecast"
+          element={
+            <DashboardLayout>
+              <MealForecastPage />
+            </DashboardLayout>
+          }
+        />
+        <Route
+          path="/team1/mealTicket"
+          element={
+            <DashboardLayout>
+              <MealTicket />
+            </DashboardLayout>
+          }
+        />
+        <Route
+          path="/team1/pay"
+          element={
+            <DashboardLayout>
+              <PayPage />
+            </DashboardLayout>
+          }
+        />
+        <Route
+          path="/team1/pay/success"
+          element={
+            <DashboardLayout>
+              <Success />
+            </DashboardLayout>
+          }
+        />
+        <Route
+          path="/team1/pay/fail"
+          element={
+            <DashboardLayout>
+              <Fail />
+            </DashboardLayout>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
